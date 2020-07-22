02973408cf0aaf85a8bcfda28abefa337cbae0e3
“dinadeljanin” <ddeljanin5@icloud.com>
2020-07-22 14:11:14 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,16 @@
 // create your App component here
+import React, {Component} from 'react'
+
+export default class App extends Component {
+  componentDidMount() {
+    fetch("http://api.open-notify.org/astros.json")
+    .then(r => r.json())
+    .then(astros => console.log(astros))
+  }
+
+  render() {
+    return (
+      <div>Hello World!</div>
+    )
+  }
+}
